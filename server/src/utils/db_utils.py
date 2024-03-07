1e82816f0052c466ffdf8107386fb3da4a9afa33
Ayush <ayushkhanduri2612@gmail.com>
2024-03-07 18:24:53 +0530

    Add docker compose for web and api containers

--- server/src/utils/db_utils.py
+++ server/src/utils/db_utils.py
@@ -1,13 +1,15 @@
+import json
+
 import firebase_admin
 from firebase_admin import credentials, firestore_async
 
 from pinecone.grpc import PineconeGRPC as Pinecone
 
-from src.utils.globals import api_keys
+from src.utils.globals import api_keys, sms_service_account_key
 
 
 # Initialize Firebase
-cred = credentials.Certificate("./sms-service-account-key.json")
+cred = credentials.Certificate(sms_service_account_key)
 firebase_admin.initialize_app(cred)
 db = firestore_async.client()
 
